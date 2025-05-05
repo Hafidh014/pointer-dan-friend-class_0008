ec70d0dcd96ec3b774e5a464c7f49a517ec34e77
Hafidh014 <muhammad.hafidh2006@gmail.com>
2025-05-05 11:07:52 +0700

    class siswa deference

--- friendclassSatu.cpp
+++ friendclassSatu.cpp
@@ -12,3 +12,14 @@ using namespace std;
         void setNama(string pNama);
         friend class siswa;
     };
+
+    class siswa
+    {
+        private:
+            int id;
+
+        public:
+        void setId(int pId);
+        void displayall(orang &a);
+    };
+    
